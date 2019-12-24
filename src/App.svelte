a669ccca40d2ae592f53510ba4310ee13abf96be
hyobum <hyob1203@naver.com>
2019-12-24 23:20:29 +0900

    feat: webpack

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,11 @@
 <script>
-	let items = [
-    { id: 1, name: "Milk", done: false },
-    { id: 2, name: "Bread", done: true },
-    { id: 3, name: "Eggs", done: false }
-	];
-	function remove () {
-		items.shift()
-		items = items
-	}
+	export let name;
 </script>
 
-<main>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<button on:click={remove}>REMOVE</button>
-	<ul>
-		{#each items as item (item.id)}
-		<li>{item.name}</li>
-		{/each}
-	</ul>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		color: purple;
 	}
 </style>
+
+<h1>Hello {name}!</h1>

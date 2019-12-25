77515d3a58f3b57d9489285a838b93b31289620f
hyobum <hyob1203@naver.com>
2019-12-25 23:23:31 +0900

    feat: sass 구조 추가

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,22 @@
 	export let name;
 </script>
 
-<style>
-	h1 {
-		color: purple;
+<style lang="scss">
+	.warp {
+		.contents {
+			width: 1080px;
+			margin: auto;
+			p {
+				font-size: 20px;
+			}
+		}
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<div class="warp">
+	<div class="header"></div>
+	<div class="contents">
+		{name}
+		<p>TTT</p>
+	</div>
+</div>

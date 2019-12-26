ef343a6d42181b7cb61488140a465048e0e7b125
이효범 <beomyhlee@gmail.com>
2019-12-26 18:55:43 +0900

    feat: layout 구조 생성

--- src/App.svelte
+++ src/App.svelte
@@ -3,21 +3,29 @@
 </script>
 
 <style lang="scss">
-	.warp {
-		.contents {
-			width: 1080px;
-			margin: auto;
-			p {
-				font-size: 20px;
-			}
-		}
+$xs: 0;
+$sm: 576px;
+$md: 768px;
+$lg: 992px;
+$xl: 1200px;
+
+@media (min-width: $xl) {
+	.header {
+		width: $xl;
+		margin: auto;
 	}
+	.contents {
+		width: $xl;
+		margin: auto;
+	}
+}
 </style>
 
 <div class="warp">
-	<div class="header"></div>
+	<div class="header">
+		beomy games
+	</div>
 	<div class="contents">
 		{name}
-		<p>TTT</p>
 	</div>
 </div>

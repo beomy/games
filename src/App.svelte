ed66a388c397b6bd3427fc75e8827f482fe85fb9
hyobum <hyob1203@naver.com>
2019-12-28 00:15:04 +0900

    feat: header 추가

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,31 @@
 <script lang="typescript">
+	import Header from './components/Header.svelte'
+
 	export let name: string;
 	let test: string = 'wow';
 </script>
 
-<style lang="scss">
-$xs: 0;
-$sm: 576px;
-$md: 768px;
-$lg: 992px;
-$xl: 1200px;
+<style lang="scss" global>
+	$xs: 0;
+	$sm: 576px;
+	$md: 768px;
+	$lg: 992px;
+	$xl: 1200px;
 
-@media (min-width: $xl) {
-	.header {
-		width: $xl;
-		margin: auto;
+	@media (min-width: $xl) {
+		.header {
+			width: $xl;
+			margin: auto;
+		}
+		.contents {
+			width: $xl;
+			margin: auto;
+		}
 	}
-	.contents {
-		width: $xl;
-		margin: auto;
-	}
-}
 </style>
 
 <div class="warp">
-	<div class="header">
-		beomy games
-	</div>
+	<Header/>
 	<div class="contents">
 		{name}
 		{test}
---
+++ src/components/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<style lang="scss">
+  .logo-link {
+    display: inline-block;
+    padding: 19px 0;
+  }
+  .logo-img-box {
+    width: 58px;
+    img {
+      width: 100%;
+    }
+  }
+</style>
+
+<div class="header">
+  <a class="logo-link" href="/" target="_blank">
+    <div class="logo-img-box">
+      <img src="images/logo.png" alt="로고" />
+    </div>
+  </a>
+  <a class="logo-link" href="/" target="_blank">
+    <div class="logo-img-box">
+      <img src="images/games_logo.png" alt="게임 로고" />
+    </div>
+  </a>
+</div>

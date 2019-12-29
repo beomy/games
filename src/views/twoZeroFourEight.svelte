c0b0a27af6a6d2d5af324967fc50497493bf5d8a
hyobum <hyob1203@naver.com>
2019-12-30 00:45:07 +0900

    feat: 2048 게임 UI 개발

--- src/views/twoZeroFourEight.svelte
+++ src/views/twoZeroFourEight.svelte
@@ -1,3 +1,49 @@
-<div>
-  TEST
+<script lang="ts">
+  const gridCount: Array<Number> = [ 1, 2, 3, 4 ];
+</script>
+
+<style lang="scss">
+  .game-container {
+    position: relative;
+    padding: 15px;
+    cursor: default;
+    touch-action: none;
+    background: #bbada0;
+    border-radius: 6px;
+    width: 500px;
+    height: 500px;
+    box-sizing: border-box;
+  }
+  .grid-container {
+    position: absolute;
+    z-index: 1;
+  }
+  .grid-row {
+    margin-bottom: 15px;
+    &:after {
+      content: "";
+      display: block;
+      clear: both;
+    }
+  }
+  .grid-cell {
+    width: 106.25px;
+    height: 106.25px;
+    margin-right: 15px;
+    float: left;
+    border-radius: 3px;
+    background: rgba(238, 228, 218, 0.35);
+  }
+</style>
+
+<div class="game-container">
+  <div class="grid-container">
+    {#each gridCount as row}
+      <div class="grid-row">
+        {#each gridCount as cell}
+          <div class="grid-cell"></div>
+        {/each}
+      </div>
+    {/each}
+  </div>
 </div>

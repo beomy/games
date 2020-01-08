e16aed009565d9d1efe5c5173bcd220b58d381ac
이효범 <beomyhlee@gmail.com>
2020-01-08 17:41:37 +0900

    #2 bugfix: 헤더 링크 이동 버그 수정

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -16,7 +16,7 @@
     <img src="./images/logo.png" alt="로고" />
   </div>
 </a>
-<a class="logo-link" href="/">
+<a class="logo-link" href="/games">
   <div class="logo-img-box">
     <img src="./images/games_logo.png" alt="게임 로고" />
   </div>
